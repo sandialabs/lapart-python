dd58bb6302bc9a6de05e45d68c487ee926ca3150
cbjones <cbjones@sandia.gov>
2017-07-07 15:14:53 -0600

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 import shlex
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('./lapart-python/lapart'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 
 
 # -- General configuration ------------------------------------------------
