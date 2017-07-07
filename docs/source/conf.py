151af4b4c5b2f0ee4d24480463e7042635c1146b
cbjones <cbjones@sandia.gov>
2017-07-07 13:29:45 -0600

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
+import shlex
 # sys.path.insert(0, os.path.abspath('.'))
 
 
