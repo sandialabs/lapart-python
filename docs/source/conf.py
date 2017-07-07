e3fbd8f2534019951ed557cae108b20c6638996c
cbjones <cbjones@sandia.gov>
2017-07-07 13:31:09 -0600

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 import os
 import sys
 import shlex
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- General configuration ------------------------------------------------
